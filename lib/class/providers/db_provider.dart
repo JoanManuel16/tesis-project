8e9c1c083e6fdac5c396ce72af684d51b20a8282
Electro-Zombie <electrozombie901@gmail.com>
2022-09-06 10:28:46 -0400

    base de datos integrada a la mitad

---
+++ lib/class/providers/db_provider.dart
@@ -0,0 +1,18 @@
+import 'package:sqflite/sqflite.dart';
+
+class DBProvider {
+  static Database _database;
+  static final DBProvider db = DBProvider._();
+
+  DBProvider._();
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database;
+    } else {
+      _database = await initDB();
+
+      return _database;
+    }
+  }
+}
